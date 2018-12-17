e8255eecf1db8c25b63eec408434ee4bb278dd85
gcorso <gabri.corso@gmail.com>
2018-12-17 21:42:09 +0100

    Hosting suggestions icons changed

--- app/src/main/java/com/gcappslab/gcorso/letsbookmark/Activities/SuggestionsActivity.java
+++ app/src/main/java/com/gcappslab/gcorso/letsbookmark/Activities/SuggestionsActivity.java
@@ -63,37 +63,37 @@ public class SuggestionsActivity extends AppCompatActivity {
 
         // Adding child data
         List<Site> news = new ArrayList<Site>();
-        news.add(new Site("CNN", "http://www.cnn.com/", "http://www.gcappslab.com/suggestions/cnn.png"));
-        news.add(new Site("NYTimes", "http://www.nytimes.com/", "http://www.gcappslab.com/suggestions/nytimes.png"));
-        news.add(new Site("NBC News", "http://www.nbcnews.com/", "http://www.gcappslab.com/suggestions/nbcnews.png"));
-        news.add(new Site("Washington Post", "http://www.washingtonpost.com/", "http://www.gcappslab.com/suggestions/washingtonpost.png"));
-        news.add(new Site("The Guardian", "http://www.theguardian.com/", "http://www.gcappslab.com/suggestions/theguardian.png"));
-        news.add(new Site("Wall Street Journal", "http://www.wsj.com/", "http://www.gcappslab.com/suggestions/wallstreetjournal.png"));
-        news.add(new Site("ABC News", "http://www.abcnews.go.com/", "http://www.gcappslab.com/suggestions/abcnews.png"));
-        news.add(new Site("BBC News", "http://www.bbc.co.uk/", "http://www.gcappslab.com/suggestions/bbcnews.png"));
-        news.add(new Site("USA Today", "http://www.usatoday.com/", "http://www.gcappslab.com/suggestions/usatoday.png"));
+        news.add(new Site("CNN", "http://www.cnn.com/", "http://gcorso.000webhostapp.com/suggestions/cnn.png"));
+        news.add(new Site("NYTimes", "http://www.nytimes.com/", "http://gcorso.000webhostapp.com/suggestions/nytimes.png"));
+        news.add(new Site("NBC News", "http://www.nbcnews.com/", "http://gcorso.000webhostapp.com/suggestions/nbcnews.png"));
+        news.add(new Site("Washington Post", "http://www.washingtonpost.com/", "http://gcorso.000webhostapp.com/suggestions/washingtonpost.png"));
+        news.add(new Site("The Guardian", "http://www.theguardian.com/", "http://gcorso.000webhostapp.com/suggestions/theguardian.png"));
+        news.add(new Site("Wall Street Journal", "http://www.wsj.com/", "http://gcorso.000webhostapp.com/suggestions/wallstreetjournal.png"));
+        news.add(new Site("ABC News", "http://www.abcnews.go.com/", "http://gcorso.000webhostapp.com/suggestions/abcnews.png"));
+        news.add(new Site("BBC News", "http://www.bbc.co.uk/", "http://gcorso.000webhostapp.com/suggestions/bbcnews.png"));
+        news.add(new Site("USA Today", "http://www.usatoday.com/", "http://gcorso.000webhostapp.com/suggestions/usatoday.png"));
 
         List<Site> sport = new ArrayList<Site>();
-        sport.add(new Site("ESPN", "http://www.espn.com/", "http://www.gcappslab.com/suggestions/espn.png"));
-        sport.add(new Site("Bleacher Report", "http://www.bleacherreport.com/", "http://www.gcappslab.com/suggestions/bleacherreport.png"));
-        sport.add(new Site("CBS Sports", "http://www.cbssports.com/", "http://www.gcappslab.com/suggestions/cbssports.png"));
-        sport.add(new Site("Sport Illustrated", "http://www.si.com/", "http://www.gcappslab.com/suggestions/sportillustrated.png"));
-        sport.add(new Site("NBA", "http://www.nba.com/", "http://www.gcappslab.com/suggestions/nba.png"));
-        sport.add(new Site("NFL", "http://www.nfl.com/", "http://www.gcappslab.com/suggestions/nfl.png"));
+        sport.add(new Site("ESPN", "http://www.espn.com/", "http://gcorso.000webhostapp.com/suggestions/espn.png"));
+        sport.add(new Site("Bleacher Report", "http://www.bleacherreport.com/", "http://gcorso.000webhostapp.com/suggestions/bleacherreport.png"));
+        sport.add(new Site("CBS Sports", "http://www.cbssports.com/", "http://gcorso.000webhostapp.com/suggestions/cbssports.png"));
+        sport.add(new Site("Sport Illustrated", "http://www.si.com/", "http://gcorso.000webhostapp.com/suggestions/sportillustrated.png"));
+        sport.add(new Site("NBA", "http://www.nba.com/", "http://gcorso.000webhostapp.com/suggestions/nba.png"));
+        sport.add(new Site("NFL", "http://www.nfl.com/", "http://gcorso.000webhostapp.com/suggestions/nfl.png"));
 
         List<Site> social = new ArrayList<Site>();
-        social.add(new Site("Facebook", "http://www.facebook.com/", "http://www.gcappslab.com/suggestions/facebook.png"));
-        social.add(new Site("Twitter", "http://www.twitter.com/", "http://www.gcappslab.com/suggestions/twitter.png"));
-        social.add(new Site("Linkedin", "http://www.linkedin.com/", "http://www.gcappslab.com/suggestions/linkedin.png"));
-        social.add(new Site("Instagram", "http://www.instagram.com/", "http://www.gcappslab.com/suggestions/instagram.png"));
-        social.add(new Site("Google+", "http://www.plus.google.com/", "http://www.gcappslab.com/suggestions/googleplus.png"));
+        social.add(new Site("Facebook", "http://www.facebook.com/", "http://gcorso.000webhostapp.com/suggestions/facebook.png"));
+        social.add(new Site("Twitter", "http://www.twitter.com/", "http://gcorso.000webhostapp.com/suggestions/twitter.png"));
+        social.add(new Site("Linkedin", "http://www.linkedin.com/", "http://gcorso.000webhostapp.com/suggestions/linkedin.png"));
+        social.add(new Site("Instagram", "http://www.instagram.com/", "http://gcorso.000webhostapp.com/suggestions/instagram.png"));
+        social.add(new Site("Google+", "http://www.plus.google.com/", "http://gcorso.000webhostapp.com/suggestions/googleplus.png"));
 
         List<Site> other = new ArrayList<Site>();
-        other.add(new Site("Amazon", "http://www.amazon.com/", "http://www.gcappslab.com/suggestions/amazon.png"));
-        other.add(new Site("EBay", "http://www.ebay.com/", "http://www.gcappslab.com/suggestions/ebay.png"));
-        other.add(new Site("Weather", "http://www.weather.com/", "http://www.gcappslab.com/suggestions/weather.png"));
-        other.add(new Site("GC Apps Lab", "http://www.gcappslab.com/", "http://www.gcappslab.com/suggestions/gc.png"));
-        other.add(new Site("Google", "http://www.google.com/", "http://www.gcappslab.com/suggestions/google.png"));
+        other.add(new Site("Amazon", "http://www.amazon.com/", "http://gcorso.000webhostapp.com/suggestions/amazon.png"));
+        other.add(new Site("EBay", "http://www.ebay.com/", "http://gcorso.000webhostapp.com/suggestions/ebay.png"));
+        other.add(new Site("Weather", "http://www.weather.com/", "http://gcorso.000webhostapp.com/suggestions/weather.png"));
+        other.add(new Site("GC Apps Lab", "http://gcorso.000webhostapp.com/", "http://gcorso.000webhostapp.com/suggestions/gc.png"));
+        other.add(new Site("Google", "http://www.google.com/", "http://gcorso.000webhostapp.com/suggestions/google.png"));
 
         listDataChild.put("News", news); // Header, Child data
         listDataChild.put("Sport", sport);
